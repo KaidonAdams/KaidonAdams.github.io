d995416635339391c369d52d75e52214b42b9c98
KaidonAdams <stu.kaidon.adams@ihsnola.org>
2021-11-22 16:55:29 +0000

    monday

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,20 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+            var circle;
+            var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+           function drawCircle(){
+            
+           }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
+            drawCircle();
+            drawCircle();
+            drawCircle();
+            drawCircle();
+            drawCircle();
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,7 +46,11 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
